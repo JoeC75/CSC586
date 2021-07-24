3365a5a189f965dc5b7d9508e55926e54100d6a0
JoeC75 <54557069+JoeC75@users.noreply.github.com>
2021-07-24 18:53:52 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,3 +1,12 @@
+import geni.portal as portal
+import geni.rspec.pg as pg
+
+pc = portal.Context()
+request = pc.makeRequestRSpec()
+
+# Add a raw PC to the request.
+node = request.XenVM("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
 # Install and execute a script that is contained in the repository.
@@ -5,3 +14,5 @@ node.addService(pg.Execute(shell="sh", command="bash /local/repository/silly.sh"
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
+
+
