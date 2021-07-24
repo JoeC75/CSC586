6a770005ccea6408aa725639d8c907d5f408c1b8
JoeC75 <54557069+JoeC75@users.noreply.github.com>
2021-07-24 18:52:55 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,33 +1,7 @@
-import geni.portal as portal
-import geni.rspec.pg as pg
-import geni.rspec.igext as IG
+node.routable_control_ip = "true"
 
-pc = portal.Context()
-request = pc.makeRequestRSpec()
+# Install and execute a script that is contained in the repository.
+node.addService(pg.Execute(shell="sh", command="bash /local/repository/silly.sh"))
 
-tourDescription = \
-"""
-This profile provides a two-node set to study SSO. One node will be the LDAP server, and the other node is a client who is to be authenticated using LDAP. 
-"""
-
-tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,tourDescription)
-request.addTour(tour)
-prefixForIP = "192.168.1."
-link = request.LAN("lan")
-
-for i in range(2):
-  if i == 0:
-    node = request.XenVM("Webserver")    
-  else:
-    node = request.XenVM("Observer")
-   
-  node.routable_control_ip = "true"  
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
